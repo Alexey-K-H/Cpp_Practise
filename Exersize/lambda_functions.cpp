5be76ceee3bb3a6b5b9ac06321a2bc420767164e
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-07-10 10:51:06 +0700

    Create lambda_functions.cpp

---
+++ Exersize/lambda_functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+//Лямбда функции
+//Стандартные алгоритмы
+
+using namespace std;
+
+void DisplayNums(vector<int>& dynArray){
+    for_each(dynArray.begin(), dynArray.end(), [](int e){
+        cout << e << " ";
+    });
+
+    cout << endl;
+}
+
+int main() {
+    vector<int> myNums;
+    myNums.push_back(501);
+    myNums.push_back(-1);
+    myNums.push_back(25);
+    myNums.push_back(-35);
+
+    DisplayNums(myNums);
+
+    cout << "Sorting from max to min" << endl;
+    //Лямбда функция сортировки
+    sort(myNums.begin(), myNums.end(), [](int num1, int num2) {return (num2 < num1);});
+    DisplayNums(myNums);
+    return 0;
+}
