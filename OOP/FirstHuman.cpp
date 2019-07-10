768366a3f08016c82ac54f300344bf2b56076f4b
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-07-10 13:40:33 +0700

    Create FirstHuman.cpp

---
+++ OOP/FirstHuman.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//Использование класса для создания приветствия
+class Human
+{
+public:
+    //свойсва
+    string name;
+    unsigned int age;
+
+    //методы
+    void IntroduseSelf(){
+        cout << "I'm " + name << " and I'm ";
+        cout << age << " year old";
+        cout << endl;
+    }
+};
+
+int main() {
+
+    //Объект Human с именем Адам
+    Human firstMan;
+    firstMan.name = "Adam";
+    firstMan.age = 30;
+
+    //Объект Human с именем Eve
+    Human* firstWoman = new Human();
+    firstWoman->name = "Eve";
+    (*firstWoman).age = 28;
+
+    firstMan.IntroduseSelf();
+    firstWoman->IntroduseSelf();
+
+    return 0;
+}
