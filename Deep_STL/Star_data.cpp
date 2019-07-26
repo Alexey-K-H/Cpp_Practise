40de12dd007d01580043b2d437b69dc2499809dc
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-07-26 14:34:37 +0700

    Update Star_data.cpp

--- Deep_STL/Star_data.cpp
+++ Deep_STL/Star_data.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <list>
+
 using namespace std;
 /*Заполнить список звезд с параметрами и отосртировать по размеру*/
-/*Еще не готов*/
+
 class Star
 {
-private:
+public:
     string name;
     string size;//размер звезды в км
     string relative_height;//относительная высота в световых годах
     string order_num;//номер появления
     string display_as;
-public:
-    Star(string& curr_name, string& curr_size, string& curr_height, string& curr_ord_num)
+
+    Star(const string& curr_name, const string& curr_size, const string& curr_height, const string& curr_ord_num)
     {
         name = curr_name;
         size = curr_size;
         relative_height = curr_height;
         order_num = curr_ord_num;
-        display_as = ("Name:" + name + " size:" + size + " height:" + relative_height + " number:" + order_num);
-    }
-
-    //оператор сравнения для сортировки
-    bool operator <(const Star& star_to_compare) const {
-        return (this->size < star_to_compare.size);
+        display_as = ("Name:" + name + " size:" + size + "(km) height:" + relative_height + "(light years) number_appearance:" + order_num);
     }
 
     //для вывода в cout
@@ -34,7 +29,12 @@ public:
     }
 };
 
-void Fill_list(list<Star> user_list)
+bool Sort_on_size(const Star& star1, const Star& star2)
+{
+    return (star1.size > star2.size);
+}
+
+void Fill_list(list<Star>& user_list)
 {
     char finish = 'n';
     while(finish != 'y')
@@ -47,7 +47,7 @@ void Fill_list(list<Star> user_list)
         string star_size;
         cin >> star_size;
 
-        cout << "Enter height";
+        cout << "Enter height:";
         string star_height;
         cin >> star_height;
 
@@ -57,14 +57,24 @@ void Fill_list(list<Star> user_list)
 
         //Функция emplace_back создает сам объект а не копирует его
         user_list.emplace_back(Star(star_name, star_size, star_height, star_num));
-        
+
         cout << "Want to finish fill? Press y/n:";
         cin >> finish;
     }
 }
 
+template<typename Type>
+void Display_data(const Type& data_stars)
+{
+    for(auto element = data_stars.cbegin(); element != data_stars.cend(); ++element)
+        cout << *element << endl;
+}
+
 int main()
 {
     list<Star> data_Stars;
-    
+    Fill_list(data_Stars);
+    data_Stars.sort(Sort_on_size);//Сортировка списка по умолчанию стабильная
+    cout << "<List of stars>" << endl;
+    Display_data(data_Stars);
 }
