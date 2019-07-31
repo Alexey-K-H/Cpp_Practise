a35bf99c6c2f026d091e5b05541174bf63c0cc2a
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-07-31 12:55:50 +0700

    Create Chain_shot.cpp

---
+++ Exersize/Chain_shot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/*Вычислить цепную дробь*/
+
+float Chain_shot(int& user_value, float& result)
+{
+    for(int i = 0; i < user_value; i++)
+    {
+        result = 1 + 1/result;
+    }
+    return result;
+}
+
+int main()
+{
+    cout << "Enter user number:";
+    int user_num = 0;
+    cin >> user_num;
+    float result = 1.0;
+    result = Chain_shot(user_num, result);
+    cout << "Result is: " << result << endl;
+    return 0;
+}
