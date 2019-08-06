f9440df7207b5eeacffb7c33f19ee2a88150f16e
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-08-06 14:15:30 +0700

    Create Array.h

---
+++ Work_with_files/Matrix_to_arr/Array.h
@@ -0,0 +1,8 @@
+#ifndef TEST_ARRAY_H
+#define TEST_ARRAY_H
+
+void Create_matrix(int& raws_cnt, int& column_cnt);
+void Display_matrix(int **array, int& raw_cnt, int& column_cnt);
+void Create_array(int **array, int& raw_cnt, int& column_cnt);
+void Display_array(int *array, int& count);
+#endif
