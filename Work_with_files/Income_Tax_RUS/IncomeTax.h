fe3274a5ab1ea29f399a1bdfdf130e36e2c160a1
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-08-08 13:24:38 +0700

    Create IncomeTax.h

---
+++ Work_with_files/Income_Tax_RUS/IncomeTax.h
@@ -0,0 +1,6 @@
+#ifndef TEST_INCOMETAX_H
+#define TEST_INCOMETAX_H
+void Summary_Income();//Сумма дохода
+void Tax_deductions(float& income, int& parameter);//Налоговые вычеты
+void Tax(float& income, bool& isResident);//Сумма налога
+#endif
