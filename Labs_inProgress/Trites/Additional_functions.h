b1fdcb1b5cd25f641c44173198f25247e70db3ae
Yagdra <43428247+Yagdra-nsk@users.noreply.github.com>
2019-09-17 23:40:33 +0700

    Delete Additional_functions.h

--- Labs_inProgress/Trites/Additional_functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef TRITS_ADDITIONAL_FUNCTIONS_H
-#define TRITS_ADDITIONAL_FUNCTIONS_H
-
-//Получение размера массива, чтобы поместить в него триты
-int Get_size_array(int &count_trits);
-
-#endif
